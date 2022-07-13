47999084ebb6211cc6fe4e73906190c7d6dd867e
AkashKank <akashkank4@gmail.com>
2022-07-13 13:42:41 +0530

    Typescript Program

---
+++ Assignment_1/Q.2_Area_Of_Circle.js
@@ -0,0 +1,10 @@
+var radius = 4;
+var area = 0;
+var PI = 3.14;
+area = Area_Of_Circle(radius);
+console.log("Area of Circle is " + area);
+function Area_Of_Circle(value) {
+    var Circle_Area = 0;
+    Circle_Area = PI * radius * radius;
+    return Circle_Area;
+}
